9ba33d1f4f8cfd3658473450daa615bfd20ccb71
dianaamonrooy <dmonroym@unal.edu.co>
2023-10-02 12:14:21 -0500

    taskById fixed

--- src/swarch2023ii/resolvers/tasks.resolver.js
+++ src/swarch2023ii/resolvers/tasks.resolver.js
@@ -7,14 +7,17 @@ const resolvers = {
 	Query: {
 		allTasks: async (_, { user_id }, contextValue) =>
 		{
-			console.log(contextValue, user_id)
+			// console.log(contextValue, user_id)
 			const check = await checkAuth(contextValue, parseInt(user_id));
 			return check instanceof Error ? check : generalRequest(`${tasks_url}s/${user_id}`, 'GET')
 		},
 		taskById: async (_, { user_id, task_id }, contextValue) =>
 			{
-				const check = checkAuth(contextValue, parseInt(user_id));
-				return check instanceof Error ? check : generalRequest(`${tasks_url}/${user_id}/${task_id}`, 'GET')
+				const check = await checkAuth(contextValue, parseInt(user_id));
+				const result = await generalRequest(`${tasks_url}/${user_id}/${task_id}`, 'GET')
+				const newresult = Object.fromEntries([...Object.entries(result)].map(([k,v]) => [k.toLowerCase(),v]))
+				// console.log(newresult)
+				return check instanceof Error ? check : newresult
 			},
 	},
 	Mutation: {
